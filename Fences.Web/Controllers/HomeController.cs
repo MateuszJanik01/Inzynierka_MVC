e1d203e0c2cb8bd886fcfe404fda6505952a5878
Mateusz Janik <janikmateusz01@gmail.com>
2024-09-05 19:44:01 +0200

    Page styled, ready to styling Gallery, added Walidation script to section contact, added section contact

--- Fences.Web/Controllers/HomeController.cs
+++ Fences.Web/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Fences.ViewModels.VM;
+using Fences.Model.DataModels;
 
 namespace Fences.Web.Controllers;
 
@@ -14,11 +15,6 @@ public class HomeController : Controller
     }
 
     public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
     {
         return View();
     }
